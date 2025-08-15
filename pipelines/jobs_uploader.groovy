14af33e7416035da757edfe24c7af24a2d4152bc
RazMkhitaryan <razo96@mail.ru>
2025-08-16 00:21:20 +0400

    Initial commit

---
+++ pipelines/jobs_uploader.groovy
@@ -0,0 +1,10 @@
+node('maven') {
+    stage('Upload All Jobs') {
+        echo "Starting job uploader..."
+        sh '''
+            set -ex
+            jenkins-jobs --conf /home/razmik/jenkins/uploader.ini update /home/razmik/jenkins/jobs
+        '''
+        echo "All jobs uploaded successfully!"
+    }
+}
