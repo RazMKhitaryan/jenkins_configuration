b243e437283ea4d42c5fc7916030fa71769b4a67
RazMkhitaryan <razo96@mail.ru>
2025-08-16 12:47:01 +0400

    Refactor `jenkinsfile_upload` pipeline to simplify structure and reduce nesting

--- pipelines/jenkinsfile_upload.groovy
+++ pipelines/jenkinsfile_upload.groovy
@@ -1,23 +1,18 @@
-pipeline {
-    node('maven') {
-        environment {
-            JOBS_DIR = "${WORKSPACE}/jobs"
-            CONFIG_FILE = "${WORKSPACE}/uploader.ini"
-        }
+node('maven') {
+    env.JOBS_DIR = "${WORKSPACE}/jobs"
+    env.CONFIG_FILE = "${WORKSPACE}/uploader.ini"
 
-        stages {
-            stage('Start Upload') {
-                steps { echo "Starting Jenkins Job Uploader..." }
-            }
+    stage('Start Upload') {
+        echo "Starting Jenkins Job Uploader..."
+    }
 
-            stage('Checkout Repo') {
-                steps { git branch: 'main', url: 'https://github.com/RazMKhitaryan/jenkins_configuration.git' }
-            }
+    stage('Checkout Repo') {
+        git branch: 'main', url: 'https://github.com/RazMKhitaryan/jenkins_configuration.git'
+    }
 
-            stage('Create uploader.ini') {
-                steps {
-                    sh """
-                cat > ${CONFIG_FILE} <<'EOF'
+    stage('Create uploader.ini') {
+        sh """
+        cat > ${CONFIG_FILE} <<'EOF'
 [job_builder]
 recursive=True
 keep_descriptions=False
@@ -28,18 +23,15 @@ user=admin
 password=11bf6664b79a47770bf07d6bf18d088417
 crumb=True
 EOF
-                """
-                    sh "cat ${CONFIG_FILE}"
-                }
-            }
+        """
+        sh "cat ${CONFIG_FILE}"
+    }
 
-            stage('Run Upload Script') {
-                steps { sh "jenkins-jobs --conf ${CONFIG_FILE} update ${JOBS_DIR}" }
-            }
+    stage('Run Upload Script') {
+        sh "jenkins-jobs --conf ${CONFIG_FILE} update ${JOBS_DIR}"
+    }
 
-            stage('Finish Upload') {
-                steps { echo "Upload finished successfully!" }
-            }
-        }
+    stage('Finish Upload') {
+        echo "Upload finished successfully!"
     }
 }
