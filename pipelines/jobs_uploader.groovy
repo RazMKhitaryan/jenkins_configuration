4f8d02882d06a416083e1e3bc094151e81ba968c
RazMkhitaryan <razo96@mail.ru>
2025-08-18 01:13:36 +0400

    Add `upload_jobs` pipeline configuration in project and `upload_jobs.yaml` files.

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -1,55 +1,43 @@
-pipeline {
-    agent { label 'maven3' }
+node('maven') {
+    env.JOBS_DIR = "${WORKSPACE}/jobs"
+    env.CONFIG_FILE = "${WORKSPACE}/uploader.ini"
 
-    environment {
-        JOBS_DIR = "${WORKSPACE}/jobs"
-        CONFIG_FILE = "${WORKSPACE}/uploader.ini"
+    stage('Start Upload') {
+        echo "Starting Jenkins Job Uploader..."
     }
 
-    stages {
-        stage('Start Upload') {
-            steps {
-                echo "Starting Jenkins Job Uploader..."
-            }
-        }
+    stage('Checkout Repo') {
+        git branch: 'main', url: 'https://github.com/RazMKhitaryan/jenkins_configuration.git'
+    }
 
-        stage('Checkout Repo') {
-            steps {
-                git branch: 'main', url: 'https://github.com/RazMKhitaryan/jenkins_configuration.git'
-            }
-        }
-
-        stage('Create uploader.ini') {
-            steps {
-                script {
-                    sh """
-                    cat > ${CONFIG_FILE} <<'EOF'
+    stage('Create uploader.ini') {
+        sh """
+        cat > ${CONFIG_FILE} <<'EOF'
 [job_builder]
 recursive=True
 keep_descriptions=False
 
 [jenkins]
-url=http://172.20.0.6:8080/jenkins/
+url=http://jenkins:8080/jenkins/
 user=admin
 password=11bf6664b79a47770bf07d6bf18d088417
 crumb=True
 EOF
-                    """
-                    sh "cat ${CONFIG_FILE}"
-                }
-            }
-        }
+        """
+        sh "cat ${CONFIG_FILE}"
+    }
 
-        stage('Run Upload Script') {
-            steps {
-                sh "jenkins-jobs --conf ${CONFIG_FILE} --flush-cache update ${JOBS_DIR}"
-            }
+    stage('Run Upload Script') {
+        docker.image('python:3.11-slim').inside('--network selenoid_net1 -v ${WORKSPACE}:/workspace') {
+            sh """
+            pip install --quiet --upgrade pip
+            pip install --quiet jenkins-job-builder
+            jenkins-jobs --conf /workspace/uploader.ini update /workspace/jobs
+            """
         }
+    }
 
-        stage('Finish Upload') {
-            steps {
-                echo "Upload finished successfully!"
-            }
-        }
+    stage('Finish Upload') {
+        echo "Upload finished successfully!"
     }
 }
