2ab178024a1979e7d38a0ed7f360d2a027b14b88
RazMkhitaryan <razo96@mail.ru>
2025-08-18 00:39:19 +0400

    Update Jenkins node, URL, and remove Edge browser option in `job_ui.yaml`

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -1,10 +1,13 @@
-node('maven3') {
-    stage('Upload All Jobs') {
-        echo "Starting job uploader..."
-        sh '''
-            set -ex
-            jenkins-jobs --conf /home/razmik/jenkins/uploader.ini update /home/razmik/jenkins/jobs
-        '''
-        echo "All jobs uploaded successfully!"
+pipeline {
+    agent { label 'maven3' }
+
+    stages {
+        stage('Upload All Jobs') {
+            steps {
+                echo "Starting job uploader..."
+                sh 'jenkins-jobs --conf /home/razmik/jenkins/uploader.ini update /home/razmik/jenkins/jobs'
+                echo "All jobs uploaded successfully!"
+            }
+        }
     }
 }
