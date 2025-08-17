c73ca577694cb9a132ff5c0773303a2268f177dd
RazMkhitaryan <razo96@mail.ru>
2025-08-18 01:53:22 +0400

    Add `upload_jobs` pipeline configuration in project and `upload_jobs.yaml` files.

--- pipelines/jobs_uploader.groovy
+++ pipelines/jobs_uploader.groovy
@@ -18,23 +18,16 @@ recursive=True
 keep_descriptions=False
 
 [jenkins]
-url=http://172.20.0.5:8080/jenkins/
+url=http://45.132.17.22//jenkins/
 user=admin
-password=11bf6664b79a47770bf07d6bf18d088417
-crumb=True
+password=admin
 EOF
         """
         sh "cat ${CONFIG_FILE}"
     }
 
     stage('Run Upload Script') {
-        docker.image('python:3.11-slim').inside('--network selenoid_net1 -v ${WORKSPACE}:/workspace') {
-            sh """
-            pip install --quiet --upgrade pip
-            pip install --quiet jenkins-job-builder
-            jenkins-jobs --conf /workspace/uploader.ini update /workspace/jobs
-            """
-        }
+        sh "jenkins-jobs --conf  ${CONFIG_FILE} --flush-cache update ${JOBS_DIR}"
     }
 
     stage('Finish Upload') {
