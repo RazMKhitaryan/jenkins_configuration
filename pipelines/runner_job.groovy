b83ee1fc3a11bc85bd5baf6268477de2af7b2df8
razmik.mkhitaryan <razmik.mkhitaryan@acba.am>
2025-08-26 23:32:24 +0400

    try allure

--- pipelines/runner_job.groovy
+++ pipelines/runner_job.groovy
@@ -1,29 +1,59 @@
 node('maven') {
+
     stage('Run All Tests in Parallel') {
         parallel(
                 'API Tests': {
-                    build job: 'Api_tests', propagate: false
+                    script {
+                        build job: 'Api_tests', propagate: false
+                    }
                 },
                 'Mobile Tests': {
-                    build job: 'Mobile_tests', propagate: false
+                    script {
+                        build job: 'Mobile_tests', propagate: false
+                    }
                 },
                 'Web Tests': {
-                    build job: 'Web_tests', propagate: false
+                    script {
+                        build job: 'Web_tests', propagate: false
+                    }
                 }
         )
     }
 
     stage('Collect Allure Results') {
-        copyArtifacts(projectName: 'Api_tests', filter: 'allure-results/**', target: 'allure-results/api', flatten: true)
-        copyArtifacts(projectName: 'Mobile_tests', filter: 'allure-results/**', target: 'allure-results/mobile', flatten: true)
-        copyArtifacts(projectName: 'Web_tests', filter: 'allure-results/**', target: 'allure-results/web', flatten: true)
+        script {
+            copyArtifacts(
+                    projectName: 'Api_tests',
+                    selector: lastSuccessful(),
+                    filter: 'allure-results/**',
+                    target: 'allure-results/api',
+                    flatten: true
+            )
+            copyArtifacts(
+                    projectName: 'Mobile_tests',
+                    selector: lastSuccessful(),
+                    filter: 'allure-results/**',
+                    target: 'allure-results/mobile',
+                    flatten: true
+            )
+            copyArtifacts(
+                    projectName: 'Web_tests',
+                    selector: lastSuccessful(),
+                    filter: 'allure-results/**',
+                    target: 'allure-results/web',
+                    flatten: true
+            )
+        }
     }
 
     stage('Generate Allure Report') {
-        allure includeProperties: false, jdk: '', results: [
-                [path: 'allure-results/api'],
-                [path: 'allure-results/mobile'],
-                [path: 'allure-results/web']
-        ]
+        script {
+            allure includeProperties: false, jdk: '', results: [
+                    [path: 'allure-results/api'],
+                    [path: 'allure-results/mobile'],
+                    [path: 'allure-results/web']
+            ]
+        }
     }
+
 }
