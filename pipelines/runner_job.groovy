a7663b789b032db15b5b3e64098f4a45a9f220d2
razmik.mkhitaryan <razmik.mkhitaryan@acba.am>
2025-08-26 23:09:12 +0400

    try to add runner

--- pipelines/runner_job.groovy
+++ pipelines/runner_job.groovy
@@ -1,38 +1,29 @@
 node('maven') {
-    stages {
-        stage('Run All Tests in Parallel') {
-            parallel {
-                stage('API Tests') {
-                    steps {
-                        build job: 'Api_tests'
-                    }
+    stage('Run All Tests in Parallel') {
+        parallel(
+                'API Tests': {
+                    build job: 'Api_tests'
+                },
+                'Mobile Tests': {
+                    build job: 'Mobile_tests'
+                },
+                'Web Tests': {
+                    build job: 'Web_tests'
                 }
-                stage('Mobile Tests') {
-                    steps {
-                        build job: 'Mobile_tests'
-                    }
-                }
-                stage('Web Tests') {
-                    steps {
-                        build job: 'Web_tests'
-                    }
-                }
-            }
-        }
+        )
+    }
 
-        stage('Collect Allure Results') {
-            steps {
-                // Copy artifacts (allure-results) from all jobs
-                copyArtifacts(projectName: 'Api_tests', filter: 'allure-results/**', flatten: true)
-                copyArtifacts(projectName: 'Mobile_tests', filter: 'allure-results/**', flatten: true)
-                copyArtifacts(projectName: 'Web_tests', filter: 'allure-results/**', flatten: true)
-            }
-        }
+    stage('Collect Allure Results') {
+        copyArtifacts(projectName: 'Api_tests', filter: 'allure-results/**', target: 'allure-results/api', flatten: true)
+        copyArtifacts(projectName: 'Mobile_tests', filter: 'allure-results/**', target: 'allure-results/mobile', flatten: true)
+        copyArtifacts(projectName: 'Web_tests', filter: 'allure-results/**', target: 'allure-results/web', flatten: true)
+    }
 
-        stage('Generate Allure Report') {
-            steps {
-                allure includeProperties: false, jdk: '', results: [[path: 'allure-results']]
-            }
-        }
+    stage('Generate Allure Report') {
+        allure includeProperties: false, jdk: '', results: [
+                [path: 'allure-results/api'],
+                [path: 'allure-results/mobile'],
+                [path: 'allure-results/web']
+        ]
     }
 }
